28b17c2ad49c72ffb476e278f9b03aab31ca45df
Cristhian muñoz <u1802342@unimilitar.edu.co>
2021-11-13 12:57:33 -0500

    sas

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./App.css";
+import "../src/components/Card.css";
+import "./components/style.css";
+import Starts from "./components/Starts.jsx";
+import Cards from "./components/Cards";
+import { Switch, Route } from "react-router-dom";
+import { Redirect } from "react-router";
+function App() {
+  return (
+    <div className="App">
+      <Redirect to="/cards" />
+      <Starts></Starts>
+      <Switch>
+        <Route path="/cards">
+          <Cards />
+        </Route>
+      </Switch>
+      <Starts></Starts>
+    </div>
+  );
+}
+
+export default App;
