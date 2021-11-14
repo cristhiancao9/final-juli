78bd491f587d5bf73a7ee16efc01acf6dd01a975
Cristhian muñoz <u1802342@unimilitar.edu.co>
2021-11-13 22:32:13 -0500

    s

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -4,24 +4,23 @@ import "./Card.css";
 import data from "./data";
 import { Link } from "react-router-dom";
 function Cards() {
+  /*   var k = [];
+  k.push(data.home.map((e) => e.replace("static/media/", ""))); */
   return (
     <div className="App">
       <div>
         <p className="titulo">Creemos un lindo recuerdo de nuestro amor</p>
       </div>
       <div className="main">
-        {data.home.map(
-          (e, i, k) => (
-            (k = e.replace("static/media/", "")),
-            (
-              <div key={i} className="contenedor-card">
-                <Link to={`/teamo/${k}`}>
-                  <img className="im" src={e} alt="" />
-                </Link>
-              </div>
-            )
-          )
-        )}
+        {data.home.map((e, i, k) => (
+          /*   (k = e.replace("static/media/", "")), */
+          <div key={i} className="contenedor-card">
+            <div>{(k = e.replace("static/media/", ""))}</div>
+            <Link to={`/teamo/${k}`}>
+              <img className="im" src={e} alt="" />
+            </Link>
+          </div>
+        ))}
       </div>
     </div>
   );
